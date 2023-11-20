890d552e81c0cb4fe65d5788f464e0c1b0cdf9bb
Ahmad Nidzam Musthafa <akamhacks@gmail.com>
2023-11-20 20:23:40 +0700

    coba commit

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons'
 
 const loading = () => {
 	return (
-		<div>
-			Loading...
-		</div>
+		<DashboardSkeleton />
 	)
 }
 
