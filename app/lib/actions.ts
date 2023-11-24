def642959549bd5881829686e65cbeff9c408ad3
Ahmad Nidzam Musthafa <akamhacks@gmail.com>
2023-11-24 07:01:21 +0700

    .

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -109,10 +110,24 @@ export async function updateInvoice(
 
 export async function deleteInvoice(id: string) {
 	try {
-	  await sql`DELETE FROM invoices WHERE id = ${id}`;
+	  await sql `DELETE FROM invoices WHERE id = ${id}`;
   }
   catch (error) {
   	throw new Error (error)
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
 }
